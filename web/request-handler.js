552e5802ecf5b7b76c8b2ce1d1d6085cdfe4f516
Hack Reactor Students <accounts@hackreactor.com>
2015-02-25 16:25:53 -0800

    Return index.html on GET to /

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,29 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var httpHelp = require('./http-helpers');
+var fs = require('fs');
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  if (req.method === "GET") {
+    if (req.url === "/") {
+
+      fs.readFile('./public/index.html', function(err, data){
+        if (err) {
+          res.writeHead(404);
+          res.end(JSON.stringify(err));
+          return;
+        }
+        res.writeHead(200, httpHelp.headers);
+        res.end(data);
+      });
+
+    } else {
+      res.writeHead(404, httpHelp.headers);
+      res.end("Nope");
+    }
+  }
+
+  // res.end(archive.paths.list);
 };
