5becfe4593aaa65ad6acae761cd5842caad1db55
Hack Reactor Students <accounts@hackreactor.com>
2015-02-25 19:40:19 -0800

    Change relative paths to absolute

--- web/request-handler.js
+++ web/request-handler.js
@@ -8,24 +8,42 @@ exports.handleRequest = function (req, res) {
 
   if (req.method === "GET") {
     if (req.url === "/") {
-      httpHelp.serveAssets(res, './public/index.html', function(data){
+      httpHelp.serveAssets(res, archive.paths['public'] + '/index.html', function(data){
         res.writeHead(200, httpHelp.headers);
         res.end(data);
       });
 
     }
     else if (req.url === "/styles.css") {
-      httpHelp.serveAssets(res, './public/styles.css', function(data){
+      httpHelp.serveAssets(res, archive.paths['public'] + '/styles.css', function(data){
         httpHelp.headers['Content-Type'] = "text/css";
         res.writeHead(200, httpHelp.headers);
         res.end(data);
       });
     }
 
+    else if (req.url.indexOf('/archives/sites') !== -1) {
+      // then user is submitting a GET request for a cached site
+      res.writeHead(200, httpHelp.headers);
+      res.end()
+    }
+
     else {
       res.writeHead(404, httpHelp.headers);
       res.end("Nope");
     }
+  }
+  // handle POST requests
+  if (req.method === "POST") {
+    var result = '';
+    req.on("data", function(chunk) {
+      result += chunk;
+    });
+    req.on("end", function() {
+      httpHelp.headers['Content-Type'] = "text/html";
+      res.writeHead(200, httpHelp.headers);
+      res.end(result);
+    })
   }
 
   // res.end(archive.paths.list);
