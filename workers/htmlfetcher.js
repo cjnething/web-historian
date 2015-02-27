004a90edd9cd7cb73cfa94ef285be2b2522062f2
Hack Reactor Students <accounts@hackreactor.com>
2015-02-26 16:12:52 -0800

    Fix htmlfetcher

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,16 +1,16 @@
 var archive = require('../helpers/archive-helpers');
 var httpRequest = require('http-request');
-// exports.wrapper = function() {
-  archive.downloadUrls(function(url) {
-    console.log("processing", url);
-    archive.isURLArchived(url, function(isArchived){
-      if (!isArchived) {
-        // download the file if not archived
-        console.log('getting', url);
-        httpRequest.get(url, archive.paths['archivedSites'] + '/' + url + '.html', function(){});
-      }
-    });
+
+archive.downloadUrls(function(url) {
+  console.log("processing", url);
+  archive.isURLArchived(url, function(isArchived){
+    if (!isArchived) {
+      // download the file if not archived
+      console.log('getting', url);
+      httpRequest.get(url, archive.paths['archivedSites'] + '/' + url + '.html', function(){});
+    }
   });
+});
 // };
 
 // go through the list, if not archived, take a snapshot
